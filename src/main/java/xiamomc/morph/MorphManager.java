12423292d3a81ef8442057a56d9540479c01ded6
MATRIX-feather <midnightcarnival@outlook.com>
2022-10-07 10:23:33 +0800

    fix: 修复burnsUnderSun检测
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/morph/MorphManager.java
+++ src/main/java/xiamomc/morph/MorphManager.java
@@ -155,10 +155,11 @@ public class MorphManager extends MorphPluginObject implements IManagePlayerData
         }
 
         if (state.isFlagSet(DisguiseState.burnsUnderSun)
+                && player.getWorld().getEnvironment().equals(World.Environment.NORMAL)
                 && player.getEquipment().getHelmet() == null
                 && player.getWorld().isDayTime()
                 && player.getWorld().isClearWeather()
-                && player.getWorld().getEnvironment().equals(World.Environment.NORMAL)
+                && !player.isInWaterOrRainOrBubbleColumn()
                 && player.getLocation().getBlock().getLightFromSky() == 15)
         {
             player.setFireTicks(200);
