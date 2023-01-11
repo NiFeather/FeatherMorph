fd583172a39270cc004dbac44d00e5b2ebfc4dcf
MATRIX-feather <midnightcarnival@outlook.com>
2023-01-12 00:42:57 +0800

    enhance: 使MessageUtils#getLocale(CommandSender)在未指定sender时尝试返回服务器默认语言
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/morph/messages/MessageUtils.java
+++ src/main/java/xiamomc/morph/messages/MessageUtils.java
@@ -5,7 +5,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import xiamomc.morph.MorphPlugin;
 import xiamomc.morph.MorphPluginObject;
+import xiamomc.morph.config.ConfigOption;
+import xiamomc.morph.config.MorphConfigManager;
+import xiamomc.pluginbase.Managers.DependencyManager;
 import xiamomc.pluginbase.Messages.FormattableMessage;
 
 public class MessageUtils extends MorphPluginObject
@@ -57,12 +61,36 @@ public class MessageUtils extends MorphPluginObject
         return locale == null ? defaultValue : locale;
     }
 
+    private static MorphConfigManager configManager;
+
+    private static MorphConfigManager getConfigManager()
+    {
+        if (configManager != null)
+            return configManager;
+
+        var depMgr = DependencyManager.getInstance(MorphPlugin.getMorphNameSpace());
+        var config = depMgr.get(MorphConfigManager.class);
+
+        configManager = config;
+
+        return config;
+    }
+
+    @Nullable
+    private static String getServerLocale()
+    {
+        var config = getConfigManager();
+
+        if (config == null) return null;
+        else return config.getOrDefault(String.class, ConfigOption.LANGUAGE_CODE);
+    }
+
     @Nullable
     public static String getLocale(CommandSender sender)
     {
         if (sender instanceof Player player)
             return getLocale(player);
         else
-            return null;
+            return getServerLocale();
     }
 }
