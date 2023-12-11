460faa380f6cee21b72df3443f44a841e423051d
MATRIX-feather <midnightcarnival@outlook.com>
2023-12-11 21:40:54 +0800

    enhance: 伪装时同步equipment misc: 默认使用ServerRenderer

---
+++ src/main/java/xiamomc/morph/backends/server/renderer/network/ProtocolEquipment.java
@@ -0,0 +1,46 @@
+package xiamomc.morph.backends.server.renderer.network;
+
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.EquipmentSlot;
+import org.bukkit.craftbukkit.v1_20_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.EntityEquipment;
+import xiamomc.morph.misc.NmsRecord;
+
+public class ProtocolEquipment
+{
+    public static ObjectArrayList<Pair<EquipmentSlot, ItemStack>> toPairs(EntityEquipment equipment)
+    {
+        var list = new ObjectArrayList<Pair<EquipmentSlot, ItemStack>>();
+
+        for (org.bukkit.inventory.EquipmentSlot bukkitSlot : org.bukkit.inventory.EquipmentSlot.values())
+        {
+            list.add(toEquipmentPair(equipment, bukkitSlot));
+        }
+
+        return list;
+    }
+
+    private static EquipmentSlot toNMSSlot(org.bukkit.inventory.EquipmentSlot bukkitSlot)
+    {
+        return switch (bukkitSlot)
+        {
+            case HAND -> net.minecraft.world.entity.EquipmentSlot.MAINHAND;
+            case OFF_HAND -> net.minecraft.world.entity.EquipmentSlot.OFFHAND;
+
+            case HEAD -> net.minecraft.world.entity.EquipmentSlot.HEAD;
+            case CHEST -> net.minecraft.world.entity.EquipmentSlot.CHEST;
+            case LEGS -> net.minecraft.world.entity.EquipmentSlot.LEGS;
+            case FEET -> net.minecraft.world.entity.EquipmentSlot.FEET;
+        };
+    }
+
+    private static Pair<EquipmentSlot, ItemStack> toEquipmentPair(EntityEquipment equipment, org.bukkit.inventory.EquipmentSlot bukkitSlot)
+    {
+        var bukkitItem = equipment.getItem(bukkitSlot);
+        var nmsItem = ItemStack.fromBukkitCopy(bukkitItem);
+
+        return Pair.of(toNMSSlot(bukkitSlot), nmsItem);
+    }
+}
---
+++ src/main/java/xiamomc/morph/backends/server/renderer/network/TestPacketListener.java
@@ -0,0 +1,89 @@
+package xiamomc.morph.backends.server.renderer.network;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListeningWhitelist;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.PacketListener;
+import com.comphenix.protocol.injector.GamePhase;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.bukkit.plugin.Plugin;
+import xiamomc.morph.MorphPlugin;
+import xiamomc.morph.MorphPluginObject;
+import xiamomc.pluginbase.Annotations.Initializer;
+
+public class TestPacketListener extends MorphPluginObject implements PacketListener
+{
+    @Initializer
+    private void load()
+    {
+
+    }
+
+    /**
+     * Invoked right before a packet is transmitted from the server to the client.
+     * <p>
+     * Note that the packet may be replaced, if needed.
+     * <p>
+     * This method is executed on the main server thread by default. However, some spigot forks (like paper) schedule
+     * specific packets off the main thread. If the {@link ListenerOptions#ASYNC} option is not specified any invocation
+     * of this method will be on the main server thread.
+     *
+     * @param event - the packet that should be sent.
+     */
+    @Override
+    public void onPacketSending(PacketEvent event)
+    {
+        logger.info("SEND! " + event.getPacket().getHandle());
+    }
+
+    /**
+     * Invoked right before a received packet from a client is being processed.
+     * <p>
+     * This method will be called asynchronously (or on the netty event loop) by default. If the
+     * {@link ListenerOptions#SYNC} option is specified, the invocation of this method will be synced to the main server
+     * thread which might cause issues due to delayed packets.
+     *
+     * @param event - the packet that has been received.
+     */
+    @Override
+    public void onPacketReceiving(PacketEvent event)
+    {
+    }
+
+    /**
+     * Retrieve which packets sent by the server this listener will observe.
+     *
+     * @return List of server packets to observe, along with the priority.
+     */
+    @Override
+    public ListeningWhitelist getSendingWhitelist()
+    {
+        var builder = ListeningWhitelist.newBuilder()
+                .gamePhase(GamePhase.PLAYING);
+
+        var types = new ObjectArrayList<PacketType>();
+
+        PacketType.Play.Server.getInstance().forEach(type ->
+        {
+            logger.info("Registing type " + type);
+            types.add(type);
+        });
+
+        builder.types(types);
+
+        return builder.build();
+    }
+
+    @Override
+    public ListeningWhitelist getReceivingWhitelist()
+    {
+        return ListeningWhitelist.EMPTY_WHITELIST;
+    }
+
+    @Override
+    public Plugin getPlugin()
+    {
+        return MorphPlugin.getInstance();
+    }
+
+}
