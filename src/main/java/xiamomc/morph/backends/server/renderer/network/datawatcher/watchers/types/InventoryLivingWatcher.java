0af906f1f2d155d9c5bd2c44c7c9786dfff533f5
MATRIX-feather <midnightcarnival@outlook.com>
2023-12-14 16:17:35 +0800

    Armor Stand, InventoryLiving

--- src/main/java/xiamomc/morph/backends/server/renderer/network/datawatcher/watchers/types/InventoryLivingWatcher.java
+++ src/main/java/xiamomc/morph/backends/server/renderer/network/datawatcher/watchers/types/InventoryLivingWatcher.java
@@ -1,36 +1,17 @@
 package xiamomc.morph.backends.server.renderer.network.datawatcher.watchers.types;
 
-import com.destroystokyo.paper.ClientOption;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import xiamomc.morph.backends.server.renderer.network.PacketFactory;
-import xiamomc.morph.backends.server.renderer.network.datawatcher.ValueIndex;
 import xiamomc.morph.backends.server.renderer.network.registries.EntryIndex;
 import xiamomc.morph.backends.server.renderer.network.registries.RegistryKey;
 import xiamomc.pluginbase.Annotations.Resolved;
 
-public class PlayerWatcher extends InventoryLivingWatcher
+public class InventoryLivingWatcher extends LivingEntityWatcher
 {
-    @Override
-    protected void initRegistry()
+    public InventoryLivingWatcher(Player bindingPlayer, EntityType entityType)
     {
-        super.initRegistry();
-
-        register(ValueIndex.PLAYER);
-    }
-
-    public PlayerWatcher(Player bindingPlayer)
-    {
-        super(bindingPlayer, EntityType.PLAYER);
-    }
-
-    @Override
-    protected void doSync()
-    {
-        this.write(ValueIndex.PLAYER.SKIN, (byte)getBindingPlayer().getClientOption(ClientOption.SKIN_PARTS).getRaw());
-        this.write(ValueIndex.PLAYER.MAINHAND, (byte)getBindingPlayer().getMainHand().ordinal());
-
-        super.doSync();
+        super(bindingPlayer, entityType);
     }
 
     @Resolved(shouldSolveImmediately = true)
