81e08a5d4da09b559f35d0a4be872a82f996e289
MATRIX-feather <midnightcarnival@outlook.com>
2024-02-28 17:11:37 +0800

    add: 添加飞行权限     - can_fly和can_fly.always，具体用途不言而喻 misc: 调整默认配置模板 misc: 也许是一些小的性能优化?

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,6 +115,8 @@ bukkit {
         register(permissionRoot + "request.send")
         register(permissionRoot + "request.accept")
         register(permissionRoot + "request.deny")
+
+        register(permissionRoot + "can_fly")
     }
 
     permissions.forEach {
