7081baa61c6ae3e829a3d07cdf6b99d6ff785fe9
MATRIX-feather <midnightcarnival@outlook.com>
2024-06-30 10:25:18 +0800

    I don't know what was I doing, that forgot to add checks

--- src/main/java/xiamomc/morph/backends/server/renderer/network/listeners/AnimationPacketListener.java
+++ src/main/java/xiamomc/morph/backends/server/renderer/network/listeners/AnimationPacketListener.java
@@ -5,6 +5,9 @@ import com.comphenix.protocol.events.ListeningWhitelist;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.injector.GamePhase;
 import net.minecraft.network.protocol.game.ClientboundAnimatePacket;
+import org.bukkit.entity.Player;
+import xiamomc.morph.backends.server.renderer.network.registries.RenderRegistry;
+import xiamomc.pluginbase.Annotations.Resolved;
 
 public class AnimationPacketListener extends ProtocolListener
 {
@@ -32,10 +35,30 @@ public class AnimationPacketListener extends ProtocolListener
         onAnimationPacket(packetEvent, clientboundAnimatePacket);
     }
 
+    @Resolved(shouldSolveImmediately = true)
+    private RenderRegistry registry;
+
     private void onAnimationPacket(PacketEvent event, ClientboundAnimatePacket clientboundAnimatePacket)
     {
-        if (clientboundAnimatePacket.getAction() == ClientboundAnimatePacket.WAKE_UP)
-            event.setCancelled(true);
+        if (clientboundAnimatePacket.getAction() != ClientboundAnimatePacket.WAKE_UP)
+            return;
+
+        var sourceEntityId = clientboundAnimatePacket.getId();
+        var nmsPlayer = this.getNmsPlayerEntityFrom(event, sourceEntityId);
+
+        if (nmsPlayer == null) return;
+
+        if (!(nmsPlayer.getBukkitEntity() instanceof Player sourcePlayer)) return;
+
+        var watcher = registry.getWatcher(sourcePlayer.getUniqueId());
+
+        if (watcher == null)
+            return;
+
+        // Don't cancel for the source
+        if (event.getPlayer().equals(sourcePlayer)) return;
+
+        event.setCancelled(true);
     }
 
     @Override
