d2e63226e0835f58c3504fdd17e3e482bc7490f9
MATRIX-feather <midnightcarnival@outlook.com>
2024-09-30 15:09:42 +0800

    misc: 调整GUI图标

--- src/main/java/xiamomc/morph/misc/gui/IconLookup.java
+++ src/main/java/xiamomc/morph/misc/gui/IconLookup.java
@@ -5,9 +5,10 @@ import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import xiamomc.morph.misc.DisguiseTypes;
 import xiamomc.morph.misc.skins.PlayerSkinProvider;
 
@@ -51,6 +52,8 @@ public class IconLookup
         register(EntityType.WITHER_SKELETON, Material.WITHER_SKELETON_SKULL);
         register(EntityType.ZOMBIE, Material.ZOMBIE_HEAD);
 
+        register(EntityType.BEE, Material.HONEYCOMB);
+
         register(EntityType.PIGLIN, Material.PIGLIN_HEAD);
         register(EntityType.PIGLIN_BRUTE, Material.GOLDEN_AXE);
 
@@ -60,11 +63,20 @@ public class IconLookup
         register(EntityType.GIANT, Material.ZOMBIE_HEAD);
         register(EntityType.ILLUSIONER, Material.SPECTRAL_ARROW);
         register(EntityType.BLAZE, Material.BLAZE_ROD);
-        register(EntityType.BOGGED, Material.BROWN_MUSHROOM);
-        register(EntityType.BREEZE, Material.BREEZE_ROD);
+
+        var poisonArrow = new ItemStack(Material.TIPPED_ARROW);
+        poisonArrow.editMeta(PotionMeta.class, potionMeta ->
+        {
+            potionMeta.addCustomEffect(new PotionEffect(PotionEffectType.POISON, 1, 0), true);
+        });
+
+        register(EntityType.BOGGED.key().asString(), poisonArrow);
+
+        register(EntityType.GHAST, Material.FIRE_CHARGE);
+
+        register(EntityType.BREEZE, Material.WIND_CHARGE);
         register(EntityType.CAMEL, Material.CACTUS);
-        //register(EntityType.CAT, Material.STRING);
-        register(EntityType.CHICKEN, Material.FEATHER);
+        register(EntityType.CHICKEN, Material.CHICKEN);
         register(EntityType.COD, Material.COD);
         register(EntityType.COW, Material.MILK_BUCKET);
         register(EntityType.DROWNED, Material.TRIDENT);
@@ -81,26 +93,32 @@ public class IconLookup
         register(EntityType.PHANTOM, Material.PHANTOM_MEMBRANE);
         register(EntityType.PIG, Material.PORKCHOP);
         register(EntityType.PILLAGER, Material.CROSSBOW);
-        //register(EntityType.POLAR_BEAR, Material.SNOWBALL);
         register(EntityType.PUFFERFISH, Material.PUFFERFISH);
         register(EntityType.RABBIT, Material.RABBIT_FOOT);
         register(EntityType.SALMON, Material.SALMON);
         register(EntityType.SHEEP, Material.WHITE_WOOL);
         register(EntityType.SHULKER, Material.SHULKER_SHELL);
-        //register(EntityType.SKELETON_HORSE, Material.BONE);
         register(EntityType.SLIME, Material.SLIME_BALL);
         register(EntityType.SNIFFER, Material.SNIFFER_EGG);
         register(EntityType.SNOW_GOLEM, Material.SNOWBALL);
         register(EntityType.SPIDER, Material.SPIDER_EYE);
         register(EntityType.SQUID, Material.INK_SAC);
-        register(EntityType.STRAY, Material.TIPPED_ARROW);
+
+        var slownessArrow = new ItemStack(Material.TIPPED_ARROW);
+        slownessArrow.editMeta(PotionMeta.class, potionMeta ->
+        {
+            potionMeta.addCustomEffect(new PotionEffect(PotionEffectType.SLOWNESS, 1, 0), false);
+        });
+
+        register(EntityType.STRAY.key().asString(), slownessArrow);
+
         register(EntityType.STRIDER, Material.WARPED_FUNGUS_ON_A_STICK);
-        register(EntityType.TROPICAL_FISH, Material.TROPICAL_FISH_BUCKET);
+        register(EntityType.TROPICAL_FISH, Material.TROPICAL_FISH);
         register(EntityType.TURTLE, Material.TURTLE_EGG);
         register(EntityType.WARDEN, Material.SCULK_CATALYST);
         register(EntityType.WITCH, Material.SPLASH_POTION);
-        register(EntityType.WITHER, Material.WITHER_ROSE);
-        register(EntityType.WOLF, Material.BONE);
+        register(EntityType.WITHER, Material.NETHER_STAR);
+        register(EntityType.WOLF, Material.WOLF_ARMOR);
         register(EntityType.ZOMBIFIED_PIGLIN, Material.GOLDEN_SWORD);
 
         register(EntityType.GOAT, Material.GOAT_HORN);
