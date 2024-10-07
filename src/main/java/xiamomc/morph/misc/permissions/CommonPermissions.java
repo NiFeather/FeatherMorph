f0d6a6bda02dd98ea3e4c8bb75c374d5e71bc437
MATRIX-feather <midnightcarnival@outlook.com>
2024-10-08 00:00:51 +0800

    misc: Let's turn it into skill item instead

--- src/main/java/xiamomc/morph/misc/permissions/CommonPermissions.java
+++ src/main/java/xiamomc/morph/misc/permissions/CommonPermissions.java
@@ -30,7 +30,7 @@ public class CommonPermissions
 
     public final static String ACCESS_SKIN_CACHE = PERM_ROOT + "skin_cache";
 
-    public final static String GIVE_SKILL_ITEM = PERM_ROOT + "give_skill_item";
+    public final static String MAKE_SKILL_ITEM = PERM_ROOT + "make_skill_item";
 
     public final static String SET_BACKEND = PERM_ROOT + "switch_backend";
 
