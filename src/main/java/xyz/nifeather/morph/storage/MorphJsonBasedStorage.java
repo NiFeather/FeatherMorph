93fe9dfa9a8a1f888108c61efe4680291764c798
MATRIX-feather <midnightcarnival@outlook.com>
2025-03-10 19:23:42 +0800

    fix build

--- src/main/java/xyz/nifeather/morph/storage/MorphJsonBasedStorage.java
+++ src/main/java/xyz/nifeather/morph/storage/MorphJsonBasedStorage.java
@@ -1,8 +1,9 @@
 package xyz.nifeather.morph.storage;
 
+import xiamomc.pluginbase.storage.JsonBasedStorage;
 import xyz.nifeather.morph.FeatherMorphMain;
 
-public abstract class MorphJsonBasedStorage<T> extends xiamomc.pluginbase.storage.JsonBasedStorage<T, FeatherMorphMain>
+public abstract class MorphJsonBasedStorage<T> extends JsonBasedStorage<T, FeatherMorphMain>
 {
     @Override
     protected String getPluginNamespace()
