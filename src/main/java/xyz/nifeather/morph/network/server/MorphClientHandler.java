1f5ccb10d818c69f15c7ad5b09ba0160f6fc6d5b
MATRIX-feather <midnightcarnival@outlook.com>
2025-04-01 01:08:18 +0800

    fix: Disconnecting a player without the mod will throw exception

--- src/main/java/xyz/nifeather/morph/network/server/MorphClientHandler.java
+++ src/main/java/xyz/nifeather/morph/network/server/MorphClientHandler.java
@@ -121,7 +121,7 @@ public class MorphClientHandler extends MorphPluginObject implements BasicClient
         if (channel == null || player == null || buffer == null)
             throw new IllegalArgumentException("Null channel/player/message");
 
-        if (!player.isOnline()) return;
+        if (!player.isOnline() || !isPlayerInitialized(player)) return;
 
         if (logOutGoingPackets.get())
             logPacket(true, player, channel, buffer.array());
