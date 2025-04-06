3a0675af30fd5fd1ea3470d973325848c5eda9d9
MATRIX-feather <midnightcarnival@outlook.com>
2025-04-07 00:08:04 +0800

    fix: Legacy message channels are not registered

--- src/main/java/xyz/nifeather/morph/network/server/MorphClientHandler.java
+++ src/main/java/xyz/nifeather/morph/network/server/MorphClientHandler.java
@@ -228,6 +228,8 @@ public class MorphClientHandler extends MorphPluginObject implements BasicClient
         messenger.registerOutgoingPluginChannel(plugin, MessageChannel.initializeChannel);
         messenger.registerOutgoingPluginChannel(plugin, MessageChannel.versionChannel);
         messenger.registerOutgoingPluginChannel(plugin, MessageChannel.commandChannel);
+        messenger.registerOutgoingPluginChannel(plugin, MessageChannel.versionChannelLegacy);
+        messenger.registerOutgoingPluginChannel(plugin, MessageChannel.commandChannelLegacy);
 
         configManager.bind(allowClient, ConfigOption.ALLOW_CLIENT);
         configManager.bind(forceTargetVersion, ConfigOption.FORCE_TARGET_VERSION);
