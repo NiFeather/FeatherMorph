cb93bc3896ffa4e3af17afdd99013349cbebf5e9
MATRIX-feather <midnightcarnival@outlook.com>
2025-04-29 19:55:56 +0800

    rm: 移除不需要的东西

--- command-compat-layer/src/main/java/xyz/nifeather/netherite/LegacyCommandProcessor.java
+++ command-compat-layer/src/main/java/xyz/nifeather/netherite/LegacyCommandProcessor.java
@@ -1,24 +1,11 @@
 package xyz.nifeather.netherite;
 
-import org.jetbrains.annotations.Nullable;
-import xyz.nifeather.netherite.network.BasicClientHandler;
-import xyz.nifeather.netherite.network.InitializeState;
-import xyz.nifeather.netherite.network.PlayerOptions;
 import xyz.nifeather.netherite.network.commands.C2S.*;
 import xyz.nifeather.netherite.network.commands.CommandRegistries;
-import xyz.nifeather.netherite.network.commands.S2C.NetheriteS2CCommand;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
-import java.util.logging.Logger;
-
-public class LegacyCommandProcessor<TPlayer> implements BasicClientHandler<TPlayer>
+public class LegacyCommandProcessor<TPlayer>
 {
     private final CommandRegistries registries = new CommandRegistries();
-
-    private final Logger logger = Logger.getLogger("FeatherMorph - Netherite");
 
     public LegacyCommandProcessor()
     {
@@ -53,125 +40,4 @@ public class LegacyCommandProcessor<TPlayer> implements BasicClientHandler<TPlay
         c2sCommand.setOwner(owner);
         return c2sCommand;
     }
-
-    private final Map<HandleType<?>, Consumer<?>> handles = new ConcurrentHashMap<>();
-
-    public <X> void setHandle(HandleType<X> handleType, Consumer<X> consumer)
-    {
-        handles.put(handleType, consumer);
-    }
-
-    public <X> void processHandle(HandleType<X> handleType, X commandInstance)
-    {
-        var handle = (Consumer<Object>) handles.getOrDefault(handleType, null);
-        if (handle != null) handle.accept(commandInstance);
-    }
-
-    @Override
-    public void onInitialCommand(NetheriteC2SInitialCommand netheriteC2SInitialCommand)
-    {
-        this.processHandle(HandleType.REQUEST_INITIAL, netheriteC2SInitialCommand);
-    }
-
-    @Override
-    public void onMorphCommand(NetheriteC2SMorphCommand netheriteC2SMorphCommand)
-    {
-        this.processHandle(HandleType.REQUEST_MORPH, netheriteC2SMorphCommand);
-    }
-
-    @Override
-    public void onOptionCommand(NetheriteC2SOptionCommand netheriteC2SOptionCommand)
-    {
-        this.processHandle(HandleType.SET_SINGLE_OPTION, netheriteC2SOptionCommand);
-    }
-
-    @Override
-    public void onSkillCommand(NetheriteC2SSkillCommand netheriteC2SSkillCommand)
-    {
-        this.processHandle(HandleType.ACTIVATE_SKILL, netheriteC2SSkillCommand);
-    }
-
-    @Override
-    public void onToggleSelfCommand(NetheriteC2SToggleSelfCommand netheriteC2SToggleSelfCommand)
-    {
-        this.processHandle(HandleType.TOGGLE_SELF_VIEW, netheriteC2SToggleSelfCommand);
-    }
-
-    @Override
-    public void onUnmorphCommand(NetheriteC2SUnmorphCommand netheriteC2SUnmorphCommand)
-    {
-        this.processHandle(HandleType.REQUEST_UNMORPH, netheriteC2SUnmorphCommand);
-    }
-
-    @Override
-    public void onRequestCommand(NetheriteC2SRequestCommand netheriteC2SRequestCommand)
-    {
-        this.processHandle(HandleType.PROCESS_EXCHANGE_REQUEST, netheriteC2SRequestCommand);
-    }
-
-    @Override
-    public void onAnimationCommand(NetheriteC2SAnimationCommand netheriteC2SAnimationCommand)
-    {
-        this.processHandle(HandleType.PLAY_ANIMATION, netheriteC2SAnimationCommand);
-    }
-
-    //region Never Implement
-
-    public static class NotImplementedException extends RuntimeException
-    {
-        public NotImplementedException(String message)
-        {
-            super(message);
-        }
-    }
-
-    @Override
-    public int getPlayerVersion(TPlayer player)
-    {
-        throw new NotImplementedException("getPlayerVersion is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public List<TPlayer> getConnectedPlayers()
-    {
-        throw new NotImplementedException("getConnectedPlayers is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public InitializeState getInitializeState(TPlayer player)
-    {
-        throw new NotImplementedException("getInitializeState is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public boolean isPlayerInitialized(TPlayer player)
-    {
-        throw new NotImplementedException("isPlayerInitialized is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public boolean isPlayerConnected(TPlayer player)
-    {
-        throw new NotImplementedException("isPlayerConnected is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public void disconnect(TPlayer player)
-    {
-        throw new NotImplementedException("disconnect is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public @Nullable PlayerOptions<TPlayer> getPlayerOption(TPlayer player)
-    {
-        throw new NotImplementedException("getPlayerOption is not implemented for LegacyCommandHub");
-    }
-
-    @Override
-    public boolean sendCommand(TPlayer player, NetheriteS2CCommand<?> netheriteS2CCommand)
-    {
-        throw new NotImplementedException("sendCommand is not implemented for LegacyCommandHub");
-    }
-
-    //endregion Never Implement
 }
