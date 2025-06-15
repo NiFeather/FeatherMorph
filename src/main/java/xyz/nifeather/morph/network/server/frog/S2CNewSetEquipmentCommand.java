d38b9b8f131df4cfc2ea10dee2d2cf83f241fc18
MATRIX-feather <midnightcarnival@outlook.com>
2025-06-15 16:38:02 +0800

    add: Add count and armor trim section for new equipment command

--- src/main/java/xyz/nifeather/morph/network/server/frog/S2CNewSetEquipmentCommand.java
+++ src/main/java/xyz/nifeather/morph/network/server/frog/S2CNewSetEquipmentCommand.java
@@ -1,7 +1,10 @@
 package xyz.nifeather.morph.network.server.frog;
 
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ArmorMeta;
 import xyz.nifeather.morph.network.BasicServerHandler;
 import xyz.nifeather.morph.network.commands.S2C.AbstractS2CCommand;
 import xyz.nifeather.morph.network.commands.S2C.set.S2CSetFakeEquipCommand;
@@ -9,6 +12,7 @@ import xyz.nifeather.morph.utilities.ItemUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class S2CNewSetEquipmentCommand extends AbstractS2CCommand<ItemStack>
 {
@@ -35,19 +39,51 @@ public class S2CNewSetEquipmentCommand extends AbstractS2CCommand<ItemStack>
     @Override
     public Map<String, String> generateArgumentMap()
     {
-        Map<String, String> enchantments = new HashMap<>();
+        Map<String, String> argumentMap = new ConcurrentHashMap<>();
+        argumentMap.put("slot", this.slot.toString());
+        argumentMap.put("type", this.item.getType().key().asString());
+        argumentMap.put("count", "" + this.item.getAmount());
+        argumentMap.put("name", ItemUtils.getItemJsonName(this.item));
 
+        Map<String, String> enchantments = new HashMap<>();
         item.getEnchantments().forEach((ench, lvl) ->
         {
             enchantments.put(ench.key().asString(), "" + lvl);
         });
 
-        return Map.of(
-                "slot", this.slot.toString(),
-                "type", this.item.getType().key().asString(),
-                "name", ItemUtils.getItemJsonName(this.item),
-                "enchantments", gson().toJson(enchantments)
-        );
+        argumentMap.put("enchantments", gson().toJson(enchantments));
+
+        appendArmorTrimIfPossible(argumentMap, this.item);
+
+        return argumentMap;
+    }
+
+    private void appendArmorTrimIfPossible(Map<String, String> argumentMap, ItemStack item)
+    {
+        if (!(item.getItemMeta() instanceof ArmorMeta armorMeta))
+            return;
+
+        var trim = armorMeta.getTrim();
+
+        if (trim == null)
+            return;
+
+        var material = RegistryAccess.registryAccess()
+                .getRegistry(RegistryKey.TRIM_MATERIAL)
+                .getKey(trim.getMaterial());
+
+        var pattern = RegistryAccess.registryAccess()
+                .getRegistry(RegistryKey.TRIM_PATTERN)
+                .getKey(trim.getPattern());
+
+        if (material == null || pattern == null)
+            return;
+
+        var stringMap = new HashMap<String, String>();
+        stringMap.put("material", material.key().asString());
+        stringMap.put("pattern", pattern.key().asString());
+
+        argumentMap.put("armor_trim", gson().toJson(stringMap));
     }
 
     private static S2CSetFakeEquipCommand.ProtocolEquipmentSlot toProtocolEquipment(EquipmentSlot slot)
