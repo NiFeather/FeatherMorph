a09908a1fd3894374410bec78fa8ed375b0fc968
MATRIX-feather <midnightcarnival@outlook.com>
2025-08-01 10:02:09 +0800

    fix: MorphAzimuthWaypointConnection 存在较大误差

--- src/main/java/xyz/nifeather/morph/misc/waypoint/connection/MorphAzimuthWaypointConnection.java
+++ src/main/java/xyz/nifeather/morph/misc/waypoint/connection/MorphAzimuthWaypointConnection.java
@@ -50,8 +50,8 @@ public class MorphAzimuthWaypointConnection implements WaypointTransmitter.Conne
         if (!player.getLocation().getWorld().equals(receiverBukkit.getLocation().getWorld()))
             return;
 
-        var vec = player.getLocation().subtract(receiverBukkit.getLocation());
-        vec = new Location(vec.getWorld(), -vec.x(), vec.y(), vec.z());
+        var vec = receiverBukkit.getLocation().subtract(player.getLocation());
+        vec = new Location(vec.getWorld(), -vec.z(), vec.y(), vec.x());
 
         float angle = (float) Math.atan2(vec.z(), vec.x());
 
