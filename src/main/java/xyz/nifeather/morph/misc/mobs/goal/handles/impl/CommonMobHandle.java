ff81416795bdf3518eb87e1958c55a584200f1ab
MATRIX-feather <midnightcarnival@outlook.com>
2025-09-06 16:30:39 +0800

    fix: 流浪商人在 MorphCommonMobAvoidPlayerGoal 激活时的速度过快

--- src/main/java/xyz/nifeather/morph/misc/mobs/goal/handles/impl/CommonMobHandle.java
+++ src/main/java/xyz/nifeather/morph/misc/mobs/goal/handles/impl/CommonMobHandle.java
@@ -4,6 +4,7 @@ import com.destroystokyo.paper.entity.ai.Goal;
 import com.destroystokyo.paper.entity.ai.VanillaGoal;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.NotNull;
 import xyz.nifeather.morph.misc.mobs.goal.AvoidPlayerGoals;
 import xyz.nifeather.morph.misc.mobs.goal.impl.MorphNearestAttackableGoal;
@@ -28,7 +29,12 @@ public class CommonMobHandle extends BasicEntityHandle<Creature>
     @Override
     protected void onTargetGoalFound(Creature creature, Goal<@NotNull Creature> vanillaGoal)
     {
-        var replacingGoal = AvoidPlayerGoals.findGoal(creature, morphManager, revealingHandler, 16, 0.8, 1.33);
+        // Dirty fix for wandering traders, we may want to detect entity's walk/sprintSpeed in future
+        boolean isWanderingTrader = creature.getType() == EntityType.WANDERING_TRADER;
+        double walkSpeed = isWanderingTrader ? 0.5 : 0.8;
+        double sprintSpeed = isWanderingTrader ? 0.5 : 1.33;
+
+        var replacingGoal = AvoidPlayerGoals.findGoal(creature, morphManager, revealingHandler, 16, walkSpeed, sprintSpeed);
         if (replacingGoal == null) return;
 
         mobGoals().addGoal(creature, getGoalPriority(creature, vanillaGoal), replacingGoal);
@@ -45,7 +51,12 @@ public class CommonMobHandle extends BasicEntityHandle<Creature>
     @Override
     protected void whenNoTargetGoal(Creature creature)
     {
-        var replacingGoal = AvoidPlayerGoals.findGoal(creature, morphManager, revealingHandler, 16, 0.8, 1.33);
+        // Dirty fix for wandering traders, we may want to detect entity's walk/sprintSpeed in future
+        boolean isWanderingTrader = creature.getType() == EntityType.WANDERING_TRADER;
+        double walkSpeed = isWanderingTrader ? 0.5 : 0.8;
+        double sprintSpeed = isWanderingTrader ? 0.5 : 1.33;
+
+        var replacingGoal = AvoidPlayerGoals.findGoal(creature, morphManager, revealingHandler, 16, walkSpeed, sprintSpeed);
         if (replacingGoal == null) return;
 
         mobGoals().addGoal(creature, 4, replacingGoal);
