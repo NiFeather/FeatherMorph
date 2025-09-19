f9a8f1f1fad6696e228376ad2429cd495fe09388
MATRIX-feather <midnightcarnival@outlook.com>
2025-09-19 18:00:14 +0800

    misc: fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("net.minecrell.plugin-yml.paper") version "0.6.0" // Generates plugin.yml
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
     id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
-    id("io.github.goooler.shadow") version "8.1.7" // Shadow PluginBase
+    id("com.gradleup.shadow") version "9.1.0" // Shadow PluginBase
 }
 
 repositories {
