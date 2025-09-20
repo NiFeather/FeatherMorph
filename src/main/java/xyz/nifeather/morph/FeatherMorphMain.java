66598d820132e986c84527699a127e150d85efd4
MATRIX-feather <midnightcarnival@outlook.com>
2025-09-20 12:10:17 +0800

    misc: Bump down to 1.21.6

--- src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
+++ src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
@@ -205,7 +205,7 @@ public final class FeatherMorphMain extends XiaMoJavaPlugin
         pluginManager = Bukkit.getPluginManager();
         var bukkitVersion = Bukkit.getMinecraftVersion();
 
-        String primaryVersion = "1.21.8";
+        String primaryVersion = "1.21.6";
         String[] compatVersions = new String[] { primaryVersion };
         if (Arrays.stream(compatVersions).noneMatch(bukkitVersion::equals))
         {
