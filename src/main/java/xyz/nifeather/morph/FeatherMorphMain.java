37dea354f02ffb00a63abd15fcda33aef40a8eec
MATRIX-feather <midnightcarnival@outlook.com>
2025-09-30 12:09:58 +0800

    misc: Allow using on 1.21.9 server, not only rc.1

--- src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
+++ src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
@@ -193,8 +193,8 @@ public final class FeatherMorphMain extends XiaMoJavaPlugin
         pluginManager = Bukkit.getPluginManager();
         var bukkitVersion = Bukkit.getMinecraftVersion();
 
-        String primaryVersion = "1.21.9 Release Candidate 1";
-        String[] compatVersions = new String[] { primaryVersion };
+        String primaryVersion = "1.21.9";
+        String[] compatVersions = new String[] { primaryVersion, "1.21.9 Release Candidate 1" };
         if (Arrays.stream(compatVersions).noneMatch(bukkitVersion::equals))
         {
             printImportantWarning(
