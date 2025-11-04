e3aeef287e88a02149e19329c8377a5304da2d75
MATRIX-feather <midnightcarnival@outlook.com>
2025-11-04 16:53:30 +0800

    enhance: 引入 ResolvableProfile 的 SkinPatch 支持; misc: 在 MorphResolvableProfileStruct 中明确声明此 profile 是否为 dynamic

--- src/main/java/xyz/nifeather/morph/misc/disguiseProperty/struct/MorphResolvableProfileStruct.java
+++ src/main/java/xyz/nifeather/morph/misc/disguiseProperty/struct/MorphResolvableProfileStruct.java
@@ -5,15 +5,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.UUID;
 
-public record MorphResolvableProfileStruct(@Nullable UUID id, @Nullable String name, List<String> properties)
+public record MorphResolvableProfileStruct(@Nullable UUID id, @Nullable String name, List<String> properties,
+                                           @Nullable String cape, @Nullable String elytra, @Nullable String model, @Nullable String bodyTexture,
+                                           boolean dynamic)
 {
-    public boolean isStatic()
-    {
-        return id != null && name != null && !properties.isEmpty();
-    }
-
-    public boolean dynamic()
-    {
-        return !isStatic();
-    }
 }
