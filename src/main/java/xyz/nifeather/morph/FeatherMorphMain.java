2fcae2c3e2d6274e2da09a831f6a72a965a9a42b
MATRIX-feather <midnightcarnival@outlook.com>
2025-12-05 11:58:17 +0800

    misc: 1.21.11-rc1

--- src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
+++ src/main/java/xyz/nifeather/morph/FeatherMorphMain.java
@@ -191,7 +191,7 @@ public final class FeatherMorphMain extends XiaMoJavaPlugin
         pluginManager = Bukkit.getPluginManager();
         var bukkitVersion = Bukkit.getMinecraftVersion();
 
-        String primaryVersion = "1.21.10";
+        String primaryVersion = "1.21.11 Release Candidate 1";
         String[] compatVersions = new String[] { primaryVersion };
         if (Arrays.stream(compatVersions).noneMatch(bukkitVersion::equals))
         {
