d3917660167000d49d62d950aed26febe23580d1
MATRIX-feather <midnightcarnival@outlook.com>
2025-12-10 18:37:09 +0800

    add: Suggestion and Random values for Zombie Nautilus Variant property

--- src/main/java/xyz/nifeather/morph/misc/disguiseProperty/values/ZombieNautilusPropertyCollection.java
+++ src/main/java/xyz/nifeather/morph/misc/disguiseProperty/values/ZombieNautilusPropertyCollection.java
@@ -1,10 +1,13 @@
 package xyz.nifeather.morph.misc.disguiseProperty.values;
 
+import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import net.minecraft.core.registries.Registries;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ZombieNautilus;
 import org.jetbrains.annotations.Nullable;
 import xyz.nifeather.morph.misc.disguiseProperty.*;
+import xyz.nifeather.morph.utilities.DisguiseUtils;
 
 import java.util.Optional;
 
@@ -13,6 +16,8 @@ public class ZombieNautilusPropertyCollection extends AbstractNautilusPropertyCo
     public final SingleProperty<ZombieNautilus.Variant> VARIANT = SingleProperty.builder(PropertyNames.ZOMBIE_NAUTILUS_VARIANT, ZombieNautilus.Variant.TEMPERATE)
             .withInputHandle(this::readVariant)
             .withOutputHandle(OutputHandles::writeKeyed)
+            .withSuggestions(RegistryAccess.registryAccess().getRegistry(RegistryKey.ZOMBIE_NAUTILUS_VARIANT).stream().map(v -> v.key().asString()).toList())
+            .withRandom(RegistryAccess.registryAccess().getRegistry(RegistryKey.ZOMBIE_NAUTILUS_VARIANT).stream().toList())
             .build();
 
     private Optional<ZombieNautilus.Variant> readVariant(String propertyName, String input)
@@ -31,5 +36,13 @@ public class ZombieNautilusPropertyCollection extends AbstractNautilusPropertyCo
     protected @Nullable ZombieNautilus tryCastEntity(@Nullable Entity targetEntity)
     {
         return targetEntity instanceof ZombieNautilus zn ? zn : null;
+    }
+
+    @Override
+    protected void setupDefaultProperties(PropertyHandler propertyHandler)
+    {
+        propertyHandler.set(VARIANT, DisguiseUtils.pick(VARIANT.randomValues()));
+
+        super.setupDefaultProperties(propertyHandler);
     }
 }
