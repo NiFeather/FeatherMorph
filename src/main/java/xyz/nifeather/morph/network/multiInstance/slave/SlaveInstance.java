7e9ad75fe0860ae14f049a0d1353baf8af170f00
MATRIX-feather <midnightcarnival@outlook.com>
2025-12-20 00:23:12 +0800

    enhance: Better handling meta sync, drop the existing before we call NetworkDataHolder#getOrCreatePlayerMeta

--- src/main/java/xyz/nifeather/morph/network/multiInstance/slave/SlaveInstance.java
+++ src/main/java/xyz/nifeather/morph/network/multiInstance/slave/SlaveInstance.java
@@ -261,14 +261,12 @@ public class SlaveInstance extends MorphPluginObject implements IInstanceService
                 continue;
 
             var offlinePlayer = Bukkit.getOfflinePlayer(Objects.requireNonNull(socketMeta.getBindingUuid(), "???"));
+
+            playerDataHolder.drop(socketMeta.getBindingUuid());
             var playerMeta = playerDataHolder.getOrCreatePlayerMeta(offlinePlayer);
 
             for (var identifier : socketMeta.getIdentifiers())
             {
-                // Avoid duplicate entries when the remote server synced data
-                if (playerMeta.getUnlockedDisguiseIdentifiers().contains(identifier))
-                    continue;
-
                 var disguiseMeta = playerDataHolder.getDisguiseMeta(identifier);
                 if (disguiseMeta == null)
                     continue;
