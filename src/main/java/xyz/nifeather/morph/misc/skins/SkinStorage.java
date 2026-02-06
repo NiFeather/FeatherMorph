d62b3f98c6546ba75a9761d7e2f020c44c652aff
MATRIX-feather <midnightcarnival@outlook.com>
2026-02-06 12:31:03 +0800

    add: Migration for legacy skin cache json file

--- src/main/java/xyz/nifeather/morph/misc/skins/SkinStorage.java
+++ src/main/java/xyz/nifeather/morph/misc/skins/SkinStorage.java
@@ -1,6 +1,8 @@
 package xyz.nifeather.morph.misc.skins;
 
+import com.google.gson.JsonParseException;
 import com.mojang.authlib.GameProfile;
+import net.minecraft.util.StringUtil;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +20,64 @@ public class SkinStorage extends DirectoryJsonBasedStorage<SingleSkin>
     protected SkinStorage()
     {
         super("skins");
+
+        migrateSkinCacheIfPossible();
+    }
+
+    private void migrateSkinCacheIfPossible()
+    {
+        var oldSkins = new File(FeatherMorphMain.getInstance().getDataFolder(), "stored_skins.json");
+        if (!oldSkins.exists()) return;
+
+        logger.info("Migrating legacy cached skins...");
+
+        SkinCacheRoot legacyCachedSkins;
+        try
+        {
+            var content = FileUtils.readFileToString(oldSkins, StandardCharsets.UTF_8);
+
+            legacyCachedSkins = gson.fromJson(content, SkinCacheRoot.class);
+        }
+        catch (JsonParseException e)
+        {
+            logger.info("Unable to parse legacy skin cache", e);
+            return;
+        }
+        catch (IOException e)
+        {
+            logger.error("Can't read file content for legacy skin cache", e);
+            return;
+        }
+
+        int skinsMigrated = 0;
+        int skinsFailedToMigrate = 0;
+
+        for (SingleSkin storedSkin : legacyCachedSkins.storedSkins)
+        {
+            if (!StringUtil.isValidPlayerName(storedSkin.name))
+            {
+                skinsFailedToMigrate++;
+                continue;
+            }
+
+            var profile = storedSkin.generateGameProfile();
+            if (profile == null)
+            {
+                skinsFailedToMigrate++;
+                continue;
+            }
+
+            if (cache(profile))
+                skinsMigrated++;
+            else
+                skinsFailedToMigrate++;
+        }
+
+        if (!oldSkins.renameTo(new File(FeatherMorphMain.getInstance().getDataFolder(), "stored_skins.migrated.json")))
+            logger.error("Can't rename skin cache file, please rename or delete it manually!");
+
+        logger.info("Done migrating legacy cached skins: %s Total, %s Moved, %s Failed".formatted(
+                legacyCachedSkins.storedSkins.size(), skinsMigrated, skinsFailedToMigrate));
     }
 
     @Override
@@ -26,7 +86,7 @@ public class SkinStorage extends DirectoryJsonBasedStorage<SingleSkin>
         return new SingleSkin();
     }
 
-    public synchronized void cache(GameProfile profile)
+    public synchronized boolean cache(GameProfile profile)
     {
         var path = this.getPath(profile.name()) + ".json";
         var file = this.directoryStorage.getFile(path, true);
@@ -35,7 +95,7 @@ public class SkinStorage extends DirectoryJsonBasedStorage<SingleSkin>
             if (FeatherMorphMain.getInstance().debugOutputEnabled())
                 logger.warn("[DEBUG] File is null, can't cache skin!");
 
-            return;
+            return false;
         }
 
         var wrapper = SingleSkin.fromProfile(profile);
@@ -44,10 +104,12 @@ public class SkinStorage extends DirectoryJsonBasedStorage<SingleSkin>
         try
         {
             FileUtils.writeStringToFile(file, json, StandardCharsets.UTF_8);
+            return true;
         }
         catch (IOException e)
         {
             logger.error("Unable to write file, can't cache skin.", e);
+            return false;
         }
     }
 
