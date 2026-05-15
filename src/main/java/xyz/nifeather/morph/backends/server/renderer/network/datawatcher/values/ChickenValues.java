4d23c545791279b51a31c10b55c75a41d62d5347
MATRIX-feather <midnightcarnival@outlook.com>
2026-05-16 00:50:53 +0800

    fix: AgeableMob disguises disconnecting nearby players

--- src/main/java/xyz/nifeather/morph/backends/server/renderer/network/datawatcher/values/ChickenValues.java
+++ src/main/java/xyz/nifeather/morph/backends/server/renderer/network/datawatcher/values/ChickenValues.java
@@ -1,5 +1,7 @@
 package xyz.nifeather.morph.backends.server.renderer.network.datawatcher.values;
 
+import com.github.retrooper.packetevents.protocol.entity.chicken.ChickenSoundVariant;
+import com.github.retrooper.packetevents.protocol.entity.chicken.ChickenSoundVariants;
 import com.github.retrooper.packetevents.protocol.entity.chicken.ChickenVariant;
 import com.github.retrooper.packetevents.protocol.entity.chicken.ChickenVariants;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
@@ -8,11 +10,13 @@ import xyz.nifeather.morph.backends.server.renderer.network.datawatcher.values.b
 public class ChickenValues extends AnimalValues
 {
     public final SingleValue<ChickenVariant> CHICKEN_VARIANT;
+    public final SingleValue<ChickenSoundVariant> CHICKEN_SOUND_VARIANT;
 
     public ChickenValues()
     {
         CHICKEN_VARIANT = createSingle("chicken_variant", ChickenVariants.TEMPERATE, EntityDataTypes.CHICKEN_VARIANT);
+        CHICKEN_SOUND_VARIANT = createSingle("chicken_sound_variant", ChickenSoundVariants.CLASSIC, EntityDataTypes.CHICKEN_SOUND_VARIANT);
 
-        registerSingle(CHICKEN_VARIANT);
+        registerSingle(CHICKEN_VARIANT, CHICKEN_SOUND_VARIANT);
     }
 }
--- src/main/java/xyz/nifeather/morph/backends/server/renderer/network/datawatcher/values/basetypes/AgeableMobValues.java
+++ src/main/java/xyz/nifeather/morph/backends/server/renderer/network/datawatcher/values/basetypes/AgeableMobValues.java
@@ -7,11 +7,12 @@ import xyz.nifeather.morph.backends.server.renderer.network.datawatcher.values.S
 public class AgeableMobValues extends MobValues
 {
     public final SingleValue<Boolean> IS_BABY = createSingle("ageable_mob_is_baby", false, EntityDataTypes.BOOLEAN);
+    public final SingleValue<Boolean> AGE_LOCKED = createSingle("ageable_mob_age_locked", false, EntityDataTypes.BOOLEAN);
 
     public AgeableMobValues()
     {
         super();
 
-        registerSingle(IS_BABY);
+        registerSingle(IS_BABY, AGE_LOCKED);
     }
 }
